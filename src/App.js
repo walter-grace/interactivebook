d3c8b2cdb22a9851c13004ee8b4113d6912e8978
walter-grace <nico.zahniser@gmail.com>
2020-05-10 18:57:06 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -64,20 +64,7 @@ class App extends Component {
                       <img className="image1" src={title} alt="title" />
                     </div>
                     <div className="App-page App-page_pink">
-                      <h6 className="h6">
- Piketty describes societal systems through the ages — such as slavery, feudalism, colonialism and caste — collectively as “inequality regimes”. 
- No surprises, then, about what he thinks is their key attribute. In each case, he uses historical sources to map the distribution of incomes and wealth and show how the situation today parallels those earlier abhorrent episodes. 
- The obvious implication: if we are not disturbed by what is going on around us, we should be.
-
-                      </h6>
-                    </div>
-                    <div className="App-page App-page_pink">
-                      <h4 className="h6">
-	Piketty believes tax loopholes can be eliminated today through international agreement and better information. 
-  Yet, if loopholes were rampant then, it undermines his argument that high progressive taxes are consistent with strong growth. 
-  If nobody actually paid those taxes in the glorious years between 1950 and 1979, we never actually ran the high-tax experiment
-
-                      </h4>
+                      <Flipper />
                     </div>
                     <div className="App-page App-page_pink">
                       <Video />
